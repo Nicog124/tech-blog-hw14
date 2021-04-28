3d0dff3a233efba42c7f885e4ae613aabb907701
Nicholas Ganim <nicog99@gmail.com>
2021-04-27 22:46:22 -0400

    trying to fix heroku

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize = process.env.blog_db
-  ? new Sequelize(process.env.blog_db)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
       dialectOptions: {
