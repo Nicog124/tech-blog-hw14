ebb8f9b89bfe8a12fbe2ae69f834b82c133f7203
Nicholas Ganim <nicog99@gmail.com>
2021-04-27 22:34:18 -0400

    forgot to commit

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+const sequelize = process.env.blog_db
+  ? new Sequelize(process.env.blog_db)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: "localhost",
+      dialect: "mysql",
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
